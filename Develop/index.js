f7b4ee72212ee616299b004d8e20d81735d63ea6
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-06-10 21:03:04 -0400

    update question types and defaults

--- Develop/index.js
+++ Develop/index.js
@@ -23,19 +23,22 @@ const questions = [{
     message: "Please write a short description of your project."
 },
 {
-    type: "input",
+    type: "list",
     name: "license",
-    message: "What kind of license should your project have?"
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
 },
 {
     type: "input",
     name: "install-command",
-    message: "What command should be run to install dependencies?"
+    message: "What command should be run to install dependencies?",
+    default: "npm i"
 },
 {
     type: "input",
     name: "tests",
-    message: "What command should be run to run tests?"
+    message: "What command should be run to run tests?",
+    default: "npm test"
 },
 {
     type: "input",
@@ -47,6 +50,8 @@ const questions = [{
     name: "contributing",
     message: "What does the user need to know about contributing to the repo?"
 }];
+
+inquirer.prompt(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
